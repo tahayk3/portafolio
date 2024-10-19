0a3357984a90ed3cdae22a853be661dda74aff68
tahayk3 <tahayk3@gmail.com>
2024-10-19 08:45:53 -0600

    agregando mas proyectos y tecnologias

--- src/pages/ProjectsPreview.jsx
+++ src/pages/ProjectsPreview.jsx
@@ -1,3 +1,5 @@
+import Technologies from "../components/Technologies/Technologies";
+import VideoComponent from "../components/VideoComponent/VideoComponent";
 import "./ProjectsPreview.css";
 const ProjectsPreview = () => {
   return (
@@ -10,6 +12,7 @@ const ProjectsPreview = () => {
             <h2>
               <a href="https://muebles502xela.netlify.app/">Muebles502Xela</a>
             </h2>
+            <Technologies items={["React", "Django", "Firebase", "Railway", "Postgresql", "Supabase", "Blender", "Axios", "Jsonwebtokens"]}/>
             <span className="top"></span>
             <span className="right"></span>
             <span className="bottom"></span>
@@ -20,6 +23,7 @@ const ProjectsPreview = () => {
             <h2>
               <a href="https://familabxela.netlify.app/">Familab</a>
             </h2>
+            <Technologies items={["Emailjs", "React"]}/>
             <span className="top"></span>
             <span className="right"></span>
             <span className="bottom"></span>
@@ -29,9 +33,36 @@ const ProjectsPreview = () => {
             <img src="./mueblesxela.png" alt="" />
             <h2>
               <a href="https://tahayk3.github.io/CatalogoMuebles/">
-                Catalogo muebles
+                Catálogo muebles
               </a>
             </h2>
+            <Technologies items={["React", "Emailjs"]}/>
+            <span className="top"></span>
+            <span className="right"></span>
+            <span className="bottom"></span>
+            <span className="left"></span>
+          </li>
+          <li className="card border shadow">
+            <img src="./portafolio.png" alt="" />
+            <h2>
+              <a href="https://tahayk3.github.io/CatalogoMuebles/">
+                Este portafolío
+              </a>
+            </h2>
+            <Technologies items={["Supabase", "React"]}/>
+            <span className="top"></span>
+            <span className="right"></span>
+            <span className="bottom"></span>
+            <span className="left"></span>
+          </li>
+          <li className="card border shadow">
+            <VideoComponent/>
+            <h2>
+              <a href="https://tahayk3.github.io/CatalogoMuebles/">
+                Api de la nasa
+              </a>
+            </h2>
+            <Technologies items={["React"]}/>
             <span className="top"></span>
             <span className="right"></span>
             <span className="bottom"></span>
